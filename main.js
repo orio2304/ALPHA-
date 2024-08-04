6805b2ca7dc870f5cd40298dcd0f24eadf7a1131
orio2304 <166709823+orio2304@users.noreply.github.com>
2024-08-04 20:16:27 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -5,7 +5,8 @@ var playButton = document.getElementById('play-button')
 
 playButton.addEventListener('click', () =>{
     playText("initiating jarvis v1 engine");
-    var a6=new Audio('mixkit-high-tech-bleep-confirmation-2520.wav')
+    var a6=new Audio('mixkit-high-tech-bleep-confirmation-2520.wav');
+    a6.play();
   });
 
   
